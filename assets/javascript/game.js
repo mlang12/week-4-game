52f1fb58050feb7829c955b5344fee5f75d5844a
Mark A Lang <mlang12@Marks-MBP.home>
2017-04-15 21:34:04 -0400

    initial commit, also created HTML and CSS files

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+//get user selection
+//move that selection to player section
+//move remaining tiles to enemies section
+//get user enemy selection
+//move that enemy to the defend area
+//make the attach button trigger a fight operation
+//  -- adjust each players life based on attack strength
+//  -- 
