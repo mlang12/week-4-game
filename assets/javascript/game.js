a0bc802804f9be9b57fda848872f4149fbd10fbc
Mark A Lang <mlang12@Marks-MBP.home>
2017-04-16 23:13:22 -0400

    refined game playability and ui

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,20 @@
-
-//get user selection
-//move that selection to player section
-//move remaining tiles to enemies section
-//get user enemy selection
-//move that enemy to the defend area
-//make the attach button trigger a fight operation
-//  -- adjust each players life based on attack strength
-//  -- 
 $(document).ready(function(){
-  var secSelect = $(".selection")
-  var secPlayer = $(".player")
-  var secEnemies = $(".enemies")
-  var secDefend = $(".defend")
+
+  //Sets the initial global variables used to select sections of the DOM
+  var secSelect = $(".selection");
+  var secPlayer = $(".player");
+  var secEnemies = $(".enemies");
+  var secDefend = $(".defend");
+  var secFight = $(".fight")
+  var resetButton = $(".reset");
+  var fightButton = $(".attack");
+  var msg = $(".message");
+  var msg2 = $(".message2");
+
+  //The entire RPG game Object
   var game = {
+
+    //The characters and their attributes
     characters: {
       han:{
         attack: 6,
@@ -35,80 +37,170 @@ $(document).ready(function(){
         defaultHp: 90
       }
     },
-    modes: ["select","enemy","fight","win","lose"],
-    mode: 'select',
-    player: '',
+
+    //Vaiables used to determine game state and character status 
+    mode: "select",  //modes: "select","enemy","fight","win","lose"
+    player: "",
+    baseAttack: 0,
     enemies: [],
-    currentEnemy: '',
+    currentEnemy: "",
+    message: "",
+    message2: "",
 
+    //Refreshes the page. No manual reset required since nothing in 
+    //the game will 'persist' to the next 'round' like a win or loss counter
     reset: function(){
-      this.player = ""
-      this.characters.han.hp = this.characters.han.defaultHp
-      this.characters.leia.hp = this.characters.leia.defaultHp
-      this.characters.darth.hp = this.characters.darth.defaultHp
-      this.characters.bobo.hp = this.characters.bobo.defaultHp
+      location.reload(); 
+    },
+
+    //Initializes the game
+    init: function(){
+      //Hide all sections that are not the select player section
+      secPlayer.hide();
+      secEnemies.hide();
+      secDefend.hide();
+      secFight.hide();
+      resetButton.hide();
 
     },
 
-    attack: function(){
-
+    //Function handles battle
+    battle: function(){
+      //Attack exchange. Decrease health by opponent's attack and increase players attack
+      this.characters[this.player].hp -= this.characters[this.currentEnemy].attack
+      this.characters[this.currentEnemy].hp -= this.characters[this.player].attack
+      this.characters[this.player].attack += this.baseAttack
     },
 
+    //Function handles what happens when a character is clicked
+    //Depending on the game state at that time
     play: function (chr){
       var _this = this //cache this
-      var char = chr.attr("id")
+      var char = chr.attr("id") //get the id of clicked character tile
+
+      //If the game is in select mode game assumes user selected a character
       if(this.mode === 'select'){
         this.player = char; //Move selected character to the 'player'
-        Object.keys(this.characters).forEach(function(nme){ //move all others to enemy
+        this.baseAttack = this.characters[char].attack; //store the base attack strength of player
+        Object.keys(this.characters).forEach(function(nme){ //move all others to enemy array
           if(nme !== _this.player) {
             _this.enemies.push(nme);
           };
         });
-        this.mode = "enemy"
-        this.redraw();
-      } else if (this.mode === "enemy"){
-        //If selection was for an enemy set to the current enemey
-        console.log(this.player, this.enemies)
-        if(this.enemies.indexOf(char) > -1){
-          this.currentEnemy = char;
-          this.enemies.splice(this.enemies.indexOf(this.currentEnemy),1);
-          this.mode = "fight"
-          this.redraw();
-        }
+        this.mode = "enemy" //Transition to enemy mode
       }
+
+      //If selection was made during enemy mode game assumes user selected who
+      //they will do battle against
+      else if (this.mode === "enemy"){
+
+        if(this.enemies.indexOf(char) > -1){ //If user clicked tile is in the enemy array
+          this.currentEnemy = char; //Move the selected character to the enemy currently in battle
+          this.enemies.splice(this.enemies.indexOf(this.currentEnemy),1); //Remove that enemy from the enemy array
+          this.mode = "fight"; //Transition to fight mode
+        };
+      };
     },
 
+    //This function is called at the end of every user input received
+    //and redraws the state of the game on the browser after each input
     redraw: function(){
-      //if(this.mode === "enemy"){
-        $("#" + this.player).appendTo(secPlayer)
+      var _this = this
+
+      if(this.mode === "enemy"){
+        $("#" + this.player).appendTo(secPlayer);
 
         this.enemies.forEach(function(en){
           $("#" + en).appendTo(secEnemies);
         });
 
+        secSelect.hide();
+        secEnemies.show();
+        secPlayer.show();
+        secFight.hide();
+        secDefend.hide();
+      }
+
+      if(this.mode === "fight"){
+        Object.keys(this.characters).forEach(function(en){
+          $("#" + en + " .hp").html("HP: " + _this.characters[en].hp);
+          $("#" + en + " .atk").html("  ||  Atk: " + _this.characters[en].attack);
+        });
+
         $("#" + this.currentEnemy).appendTo(secDefend);
-      //}          
+        msg = this.message;
+        msg2 = this.message2;
+        secEnemies.hide();
+        secFight.show();
+        secDefend.show();
+
+      }
+
+    },
+
+    //Checks if the state of the game is in 'win' or 'lose' state and
+    //handles them. Otherwise does nothing.
+    checkWin: function(){
+      if(this.mode === "win"){
+        this.message = "Yay! You won!"
+      };
+
+      if(this.mode === "lose"){
+        this.message = "Your player died."
+      };
+
+      if(this.mode === "win" || this.mode === "lose"){
+        this.message2 = "Please click restart to play again."
+        resetButton.show();
+      }
+
+    },
+
+    //Handles the state of the battle/game after exchange of attacks
+    battleResult: function(){
+      if(this.mode === "fight"){
+
+        //If player has died
+        if(this.characters[this.player].hp < 1){
+          this.mode = "lose";
+        };
+
+        //If enemy has died
+        if(this.characters[this.currentEnemy].hp < 1){
+          if(this.enemies.length === 0) {
+            this.mode = "win";
+          } else {
+            $("#" + this.currentEnemy).hide()
+            this.mode = "enemy";
+          };
+        };
+      };
     }
   }  
 
-  game.reset()
+  //Calls the game initializer
+  game.init();
 
   //If one of the character boxes is selected it passes that element
   //to the game for handling
   $('.characters').click(function(){ 
-    game.play($(this))
-  })
+    game.play($(this));
+    game.redraw();
+  });
 
-  //if the attach button is clicked it passes that info to the game 
-  //for handling
-  $('.attack').click(function(){ 
-    game.attack()
-  })
+  //if the attack button is clicked it passes that info to the game 
+  //for handling then checks to see if game is over
+  fightButton.click(function(){ 
+    game.battle();
+    game.battleResult();
+    game.checkWin();
+    game.redraw();
+  });
 
-  //If the reset button is hit it will trigger a game reset
-  $('.restart').click(function(){ 
-    game.reset()
-  })
+  //If the reset button is hit it will refresh the page
+  resetButton.click(function(){ 
+    game.reset();
+  });
 
 
 })
