a15a44a0fd7ce3599e397edb8c0df5af933da63e
Mark A Lang <mlang12@Marks-MacBook-Pro.local>
2017-04-18 11:57:18 -0400

    refined 1.0 code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-
+  "use strict";
   //Sets the initial global variables used to select sections of the DOM
   var secSelect = $(".selection");
   var secPlayer = $(".player");
   var secEnemies = $(".enemies");
   var secDefend = $(".defend");
-  var secFight = $(".fight")
+  var secFight = $(".fight");
   var resetButton = $(".reset");
   var fightButton = $(".attack");
   var msg = $(".message");
@@ -17,27 +17,27 @@ $(document).ready(function(){
     //The characters and their attributes
     characters: {
       han:{
-        attack: 6,
+        attack: 12,
+        counterAttack: 12,
         hp: 100,
-        defaultHp: 100,
         alias: "Baby Han"
       },
       leia:{
-        attack: 5,
+        attack: 10,
+        counterAttack: 10,
         hp: 110,
-        defaultHp: 110,
         alias: "Lil Leia"
       },
       darth: {
-        attack: 8,
-        hp: 85,
-        defaultHp: 85,
+        attack: 25,
+        counterAttack: 25,
+        hp: 60,
         alias: "Darthy"
       },
       bobo: {
-        attack: 7,
+        attack: 15,
+        counterAttack: 15,
         hp: 90,
-        defaultHp: 90,
         alias: "Bobo"
       }
     },
@@ -45,7 +45,6 @@ $(document).ready(function(){
     //Vaiables used to determine game state and character status 
     mode: "select",  //modes: "select","enemy","fight","win","lose"
     player: "",
-    baseAttack: 0,
     enemies: [],
     currentEnemy: "",
     message: "",
@@ -62,40 +61,41 @@ $(document).ready(function(){
 
     //Initializes the game
     init: function(){
-      //Hide all sections that are not the select player section
-      this.message = "Select Your Character"
-      this.message2 = "-***-"
+      //Set initial message and redraw
+      this.message = "Select Your Character";
+      this.message2 = "-***-";
       this.redraw();
     },
 
     //Function handles battle
     battle: function(){
+      //Create each round attack result messages
+      this.message = "You did " + this.characters[this.currentEnemy].alias + " " + this.characters[this.player].attack + " damage.";
+      this.message2 = this.characters[this.currentEnemy].alias + " attacked you back for " + this.characters[this.currentEnemy].counterAttack + ".";
+
       //Attack exchange. Decrease health by opponent's attack and increase players attack
-      this.characters[this.player].hp -= this.characters[this.currentEnemy].attack
-      this.characters[this.currentEnemy].hp -= this.characters[this.player].attack
-      this.characters[this.player].attack += this.baseAttack
-      this.message = "You did " + this.characters[this.currentEnemy].alias + " " + this.characters[this.player].attack + " damage."
-      this.message2 = this.characters[this.currentEnemy].alias + " attacked you back for " + this.characters[this.currentEnemy].attack + "."
+      this.characters[this.player].hp -= this.characters[this.currentEnemy].counterAttack;
+      this.characters[this.currentEnemy].hp -= this.characters[this.player].attack;
+      this.characters[this.player].attack += this.characters[this.player].counterAttack;
     },
 
     //Function handles what happens when a character is clicked
     //Depending on the game state at that time
     play: function (chr){
-      var _this = this //cache this
-      var char = chr.attr("id") //get the id of clicked character tile
+      var _this = this; //Cache this
+      var char = chr.attr("id"); //Get the id of clicked character tile
 
       //If the game is in select mode game assumes user selected a character
-      if(this.mode === 'select'){
+      if(this.mode === "select"){
         this.player = char; //Move selected character to the 'player'
-        this.baseAttack = this.characters[char].attack; //store the base attack strength of player
-        this.message = "Select who you want to battle against"
-        this.message2 = "-***-"
+        this.message = "Select who you want to battle against";
+        this.message2 = "-***-";
         Object.keys(this.characters).forEach(function(nme){ //move all others to enemy array
           if(nme !== _this.player) {
             _this.enemies.push(nme);
-          };
+          }
         });
-        this.mode = "enemy" //Transition to enemy mode
+        this.mode = "enemy"; //Transition to enemy mode
       }
 
       //If selection was made during enemy mode game assumes user selected who
@@ -108,15 +108,16 @@ $(document).ready(function(){
           this.message = "";
           this.message2 = "";
           this.mode = "fight"; //Transition to fight mode
-        };
-      };
+        }
+      }
     },
 
     //This function is called at the end of every user input received
     //and redraws the state of the game on the browser after each input
     redraw: function(){
-      var _this = this
+      var _this = this; //Cache this
 
+      //When the game is in select mode (this runs on startup) it hides all unused areas of the DOM
       if (this.mode === "select"){
         secPlayer.hide();
         secEnemies.hide();
@@ -127,38 +128,44 @@ $(document).ready(function(){
         secSelect.show();
       }
 
+      //Will run when mode is 'enemy.' Moves character and enemy tiles to proper locations
+      //Hides the character seleciton section, buttons, and defend areas. Shows the enemy select
+      //and player character sections. Sets up to wait for player to select battle opponent
       if(this.mode === "enemy"){
-        $("#" + this.player).appendTo(secPlayer);
-        $("#" + this.player).css('background-color', this.playerColor);
+        $("#" + this.player).appendTo(secPlayer); //Move selected player to the player section
+        $("#" + this.player).css("background-color", this.playerColor); //Color the player in playerColor
 
         this.enemies.forEach(function(en){
-          $("#" + en).appendTo(secEnemies);
-          $("#" + en).css('background-color', _this.enemyColor);
+          $("#" + en).appendTo(secEnemies); //Moves all enemies to the enemy section of DOM
+          $("#" + en).css("background-color", _this.enemyColor); //Colors enemies in enemy color
         });
 
         secSelect.hide();
-        secEnemies.show();
-        secPlayer.show();
         fightButton.hide();
         resetButton.hide();
         secDefend.hide();
+        secEnemies.show();
+        secPlayer.show();
       }
 
+      //Runs when the game is in fight mode. Moves the selected current enemy to the defend area
+      //Hides the enemy section and shows the fight button and defend areas of DOM
       if(this.mode === "fight"){
         $("#" + this.currentEnemy).appendTo(secDefend);
-        msg.html(this.message);
-        msg2.html(this.message2);
-        fightButton.show();
+
         secEnemies.hide();
+        fightButton.show();
         secDefend.show();
-      };
+      }
 
-      if(this.mode === "win" || this.mode === 'lose'){
+      //If the game has been won or lost hide the fight button and defend areas. Show the reset button.
+      if(this.mode === "win" || this.mode === "lose"){
         fightButton.hide();
-        resetButton.show();
         secDefend.hide();
-      };
+        resetButton.show();
+      }
 
+      //Regardless of game mode update each character's stats on refresh and update the message texts
       Object.keys(this.characters).forEach(function(en){
         $("#" + en + " .hp").html("HP: " + _this.characters[en].hp);
         $("#" + en + " .atk").html("  ||  Atk: " + _this.characters[en].attack);
@@ -171,15 +178,15 @@ $(document).ready(function(){
     //handles them. Otherwise does nothing.
     checkWin: function(){
       if(this.mode === "win"){
-        this.message = "Yay! You won!"
-      };
+        this.message = "Yay! You won!";
+      }
 
       if(this.mode === "lose"){
-        this.message = "Your player died."
-      };
+        this.message = "Your player, " + this.characters[this.player].alias + " has died.";
+      }
 
       if(this.mode === "win" || this.mode === "lose"){
-        this.message2 = "Please click restart to play again."
+        this.message2 = "Please click restart to play again.";
       }
 
     },
@@ -191,29 +198,30 @@ $(document).ready(function(){
         //If player has died
         if(this.characters[this.player].hp < 1){
           this.mode = "lose";
-        };
+        }
 
-        //If enemy has died
-        if(this.characters[this.currentEnemy].hp < 1){
+        //If enemy has died and you didn't die first
+        if(this.characters[this.currentEnemy].hp < 1 && this.mode !== "lose"){
           if(this.enemies.length === 0) {
             this.mode = "win";
           } else {
             this.mode = "enemy";
-          };
-          $("#" + this.currentEnemy).hide()
+          }
+
+          $("#" + this.currentEnemy).hide();
           this.message = "";
           this.message2 = "";
-        };
-      };
+        }
+      }
     }
-  }  
+  };  
 
   //Calls the game initializer
   game.init();
 
   //If one of the character boxes is selected it passes that element
   //to the game for handling
-  $('.characters').click(function(){ 
+  $(".characters").click(function(){ 
     game.play($(this));
     game.redraw();
   });
@@ -231,6 +239,4 @@ $(document).ready(function(){
   resetButton.click(function(){ 
     game.reset();
   });
-
-
-})
+});
