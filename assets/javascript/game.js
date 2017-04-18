f4d19a98a7769359a7737842f52578f348a54723
Mark A Lang <mlang12@Marks-MBP.home>
2017-04-17 22:43:01 -0400

    1.0 working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,22 +19,26 @@ $(document).ready(function(){
       han:{
         attack: 6,
         hp: 100,
-        defaultHp: 100
+        defaultHp: 100,
+        alias: "Baby Han"
       },
       leia:{
         attack: 5,
         hp: 110,
-        defaultHp: 110
+        defaultHp: 110,
+        alias: "Lil Leia"
       },
       darth: {
         attack: 8,
         hp: 85,
-        defaultHp: 85
+        defaultHp: 85,
+        alias: "Darthy"
       },
       bobo: {
         attack: 7,
         hp: 90,
-        defaultHp: 90
+        defaultHp: 90,
+        alias: "Bobo"
       }
     },
 
@@ -46,6 +50,9 @@ $(document).ready(function(){
     currentEnemy: "",
     message: "",
     message2: "",
+    playerColor: "#18d545",
+    enemyColor: "#fd4242",
+    defaultColor: "#f8f8f8",
 
     //Refreshes the page. No manual reset required since nothing in 
     //the game will 'persist' to the next 'round' like a win or loss counter
@@ -56,12 +63,9 @@ $(document).ready(function(){
     //Initializes the game
     init: function(){
       //Hide all sections that are not the select player section
-      secPlayer.hide();
-      secEnemies.hide();
-      secDefend.hide();
-      secFight.hide();
-      resetButton.hide();
-
+      this.message = "Select Your Character"
+      this.message2 = "-***-"
+      this.redraw();
     },
 
     //Function handles battle
@@ -70,6 +74,8 @@ $(document).ready(function(){
       this.characters[this.player].hp -= this.characters[this.currentEnemy].attack
       this.characters[this.currentEnemy].hp -= this.characters[this.player].attack
       this.characters[this.player].attack += this.baseAttack
+      this.message = "You did " + this.characters[this.currentEnemy].alias + " " + this.characters[this.player].attack + " damage."
+      this.message2 = this.characters[this.currentEnemy].alias + " attacked you back for " + this.characters[this.currentEnemy].attack + "."
     },
 
     //Function handles what happens when a character is clicked
@@ -82,6 +88,8 @@ $(document).ready(function(){
       if(this.mode === 'select'){
         this.player = char; //Move selected character to the 'player'
         this.baseAttack = this.characters[char].attack; //store the base attack strength of player
+        this.message = "Select who you want to battle against"
+        this.message2 = "-***-"
         Object.keys(this.characters).forEach(function(nme){ //move all others to enemy array
           if(nme !== _this.player) {
             _this.enemies.push(nme);
@@ -97,6 +105,8 @@ $(document).ready(function(){
         if(this.enemies.indexOf(char) > -1){ //If user clicked tile is in the enemy array
           this.currentEnemy = char; //Move the selected character to the enemy currently in battle
           this.enemies.splice(this.enemies.indexOf(this.currentEnemy),1); //Remove that enemy from the enemy array
+          this.message = "";
+          this.message2 = "";
           this.mode = "fight"; //Transition to fight mode
         };
       };
@@ -107,35 +117,54 @@ $(document).ready(function(){
     redraw: function(){
       var _this = this
 
+      if (this.mode === "select"){
+        secPlayer.hide();
+        secEnemies.hide();
+        secDefend.hide();
+        fightButton.hide();
+        resetButton.hide();
+        secFight.show();
+        secSelect.show();
+      }
+
       if(this.mode === "enemy"){
         $("#" + this.player).appendTo(secPlayer);
+        $("#" + this.player).css('background-color', this.playerColor);
 
         this.enemies.forEach(function(en){
           $("#" + en).appendTo(secEnemies);
+          $("#" + en).css('background-color', _this.enemyColor);
         });
 
         secSelect.hide();
         secEnemies.show();
         secPlayer.show();
-        secFight.hide();
+        fightButton.hide();
+        resetButton.hide();
         secDefend.hide();
       }
 
       if(this.mode === "fight"){
-        Object.keys(this.characters).forEach(function(en){
-          $("#" + en + " .hp").html("HP: " + _this.characters[en].hp);
-          $("#" + en + " .atk").html("  ||  Atk: " + _this.characters[en].attack);
-        });
-
         $("#" + this.currentEnemy).appendTo(secDefend);
-        msg = this.message;
-        msg2 = this.message2;
+        msg.html(this.message);
+        msg2.html(this.message2);
+        fightButton.show();
         secEnemies.hide();
-        secFight.show();
         secDefend.show();
+      };
 
-      }
+      if(this.mode === "win" || this.mode === 'lose'){
+        fightButton.hide();
+        resetButton.show();
+        secDefend.hide();
+      };
 
+      Object.keys(this.characters).forEach(function(en){
+        $("#" + en + " .hp").html("HP: " + _this.characters[en].hp);
+        $("#" + en + " .atk").html("  ||  Atk: " + _this.characters[en].attack);
+      });
+      msg.html(this.message);
+      msg2.html(this.message2);
     },
 
     //Checks if the state of the game is in 'win' or 'lose' state and
@@ -151,7 +180,6 @@ $(document).ready(function(){
 
       if(this.mode === "win" || this.mode === "lose"){
         this.message2 = "Please click restart to play again."
-        resetButton.show();
       }
 
     },
@@ -170,9 +198,11 @@ $(document).ready(function(){
           if(this.enemies.length === 0) {
             this.mode = "win";
           } else {
-            $("#" + this.currentEnemy).hide()
             this.mode = "enemy";
           };
+          $("#" + this.currentEnemy).hide()
+          this.message = "";
+          this.message2 = "";
         };
       };
     }
